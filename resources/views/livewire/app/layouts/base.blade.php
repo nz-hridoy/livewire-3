e8ad7880442f538c81b961b20e77b01113203c70
nz-hridoy <nayeemuzaman05@gmail.com>
2023-08-02 21:26:10 +0600

    navigated event

--- resources/views/livewire/app/layouts/base.blade.php
+++ resources/views/livewire/app/layouts/base.blade.php
@@ -22,5 +22,11 @@
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"></script>
+
+    <script>
+        document.addEventListener('livewire:navigated', () => {
+            console.log('navigated');
+        });
+    </script>
 </body>
 </html>
